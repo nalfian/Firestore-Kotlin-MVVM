aa82c4ef4f2940e891607a3c654c4b25b19f8767
Nur Alfian Julianda <nalfianjuli@gmail.com>
2019-09-27 17:12:49 +0700

    logic update local

--- app/src/main/java/com/nalfian/firestore_mvvm/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/nalfian/firestore_mvvm/data/repository/NoteRepositoryImpl.kt
@@ -13,24 +13,31 @@ class NoteRepositoryImpl(
 ) : NoteRepository {
 
     override suspend fun getNotes(): LiveData<out List<Note>> {
-        val notes = database.collection("note")
-            .get()
-            .await()
-        for (doc in notes.documents) {
-            Coroutines.io {
-                val note = doc.toObject(Note::class.java)
-                note?.id = doc.id
-                if (note?.date?.let { noteDao.checkIsDuplicate(it).size } == 0)
-                    note.let { noteDao.upsert(it) }
-            }
-        }
+        updateNote()
         return noteDao.getNotes()
     }
 
-    override fun createNote(note: Note) {
-        Coroutines.io { noteDao.upsert(note) }
+    private fun updateNote() {
         database.collection("note")
-            .add(note)
+            .addSnapshotListener { querySnapshot, _ ->
+                querySnapshot?.let { query ->
+                    for (doc in query.documents) {
+                        Coroutines.io {
+                            val note = doc.toObject(Note::class.java)
+                            note?.id = doc.id
+                            if (note?.date?.let { noteDao.checkIsDuplicate(it).size } == 0)
+                                note.let { noteDao.upsert(it) }
+                        }
+                    }
+                }
+            }
+    }
+
+    override fun createNote(note: Note) {
+        note.id = database.collection("note").document().id
+        database.collection("note")
+            .document(note.id)
+            .set(note)
             .addOnSuccessListener {}
     }
 
